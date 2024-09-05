eeb2d6ace9b569bc8f9d2cdd7211defef8a9bcec
Gluonmaster <realabyssalrealm@gmail.com>
2024-09-04 17:18:04 -0700

    Compiler_Script.ps1

--- Compiler_Script.ps1
+++ Compiler_Script.ps1
@@ -3,5 +3,5 @@ Set-ExecutionPolicy Unrestricted
 Install-Module ps2exe         
 Import-Module ps2exe
 cd "C:\Users\melod\Downloads"
-Invoke-ps2exe .\Installer.ps1 .\Installer2.exe -iconFile .\CelestialRealm.ico -noConsole
+Invoke-ps2exe .\Installer_Source_Code.ps1 .\Celestial_Installer.exe -iconFile .\CelestialRealm.ico -noConsole
 Set-ExecutionPolicy Restricted
